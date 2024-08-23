7873f1046f9f15b1a53aece38ab8777b839f6278
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-23 12:38:45 +0300

    updated

--- app/src/main/java/com/ismailmesutmujde/myfirstapp/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/myfirstapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ismailmesutmujde.myfirstapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeImage(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.image2);
     }
 }
